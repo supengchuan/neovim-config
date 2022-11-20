23eac9f0167bcb8d8e724d494effd104d339603e
supengchuan <supengchuan@qq.com>
2022-11-20 11:14:17 +0800

    dsiable auto close

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -4,15 +4,15 @@ local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
 local autocmd = vim.api.nvim_create_autocmd
 
 -- nvim-tree 自动关闭
-autocmd("BufEnter", {
-	nested = true,
-	group = myAutoGroup,
-	callback = function()
-		if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-			vim.cmd("quit")
-		end
-	end,
-})
+--autocmd("BufEnter", {
+--	nested = true,
+--	group = myAutoGroup,
+--	callback = function()
+--		if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+--			vim.cmd("quit")
+--		end
+--	end,
+--})
 
 -- 进入Terminal 自动进入插入模式
 autocmd("TermOpen", {

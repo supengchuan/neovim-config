94b4dd1becead61c1696a15f9d915a1ef8018fab
苏鹏川 <supengchuan@datacloak.com>
2022-11-21 12:56:52 +0800

    autocmd jump-last-position

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -54,3 +54,13 @@ autocmd("BufEnter", {
 			+ "r" -- But do continue when pressing enter.
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+		if { row, col } ~= { 0, 0 } then
+			vim.api.nvim_win_set_cursor(0, { row, 0 })
+		end
+	end,
+	group = myAutoGroup,
+})

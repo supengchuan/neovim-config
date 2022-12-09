5b01dbdb183d8192a4b75d9d62ba3311e5f41805
supengchuan <supengchuan@qq.com>
2022-12-09 19:47:09 +0800

    luasnip

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -4,17 +4,14 @@ if not status then
 	return
 end
 
-local luasnip
-status, luasnip = pcall(require, "luasnip")
+local ls
+status, ls = pcall(require, "luasnip")
 if not status then
 	vim.notify("没有找到 luasnip")
 	return
 end
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+require("cmp-config.luasnip")
 
 local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
 vim.api.nvim_create_autocmd("ModeChanged", {
@@ -22,8 +19,8 @@ vim.api.nvim_create_autocmd("ModeChanged", {
 	pattern = { "s:n", "i:*" },
 	desc = "Forget the current snippet when leaving the insert mode",
 	callback = function(evt)
-		if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
-			luasnip.unlink_current()
+		if ls.session and ls.session.current_nodes[evt.buf] and not ls.session.jump_active then
+			ls.unlink_current()
 		end
 	end,
 })
@@ -70,7 +67,9 @@ cmp.setup({
 			vim_item.menu = ({
 				buffer = "[Buffer]",
 				nvim_lsp = "[LSP]",
+				nvim_lua = "[API]",
 				luasnip = "[LuaSnip]",
+				path = "[Path]",
 				dap = "[DAP]",
 			})[entry.source.name]
 			return vim_item
@@ -89,50 +88,23 @@ cmp.setup({
 		end
 	end,
 
-	window = {
-		-- 使用这个有背景不一致的问题
-		--completion = cmp.config.window.bordered(),
-		--documentation = cmp.config.window.bordered(),
-	},
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
 
 	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		--	["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		--	["<C-f>"] = cmp.mapping.scroll_docs(4),
-		--	["<C-Space>"] = cmp.mapping.complete(),
-		--	["<C-e>"] = cmp.mapping.abort(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<tab>"] = cmp.config.disable,
+		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 
 	sources = cmp.config.sources({
+		{ nam = "nvim_lua" },
 		{ name = "nvim_lsp", group_index = 1 },
 		{ name = "luasnip", group_index = 1 }, -- For luasnip users.
 		{ name = "nvim_lsp_signature_help", group_index = 1 },
@@ -152,6 +124,38 @@ cmp.setup({
 		},
 		{ name = "path", group_index = 2 },
 	}),
+
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.scrote,
+			-- copied from cmp-under, but I don't think I need the plugin for this.
+			-- I might add some more of my own.
+			function(entry1, entry2)
+				local _, entry1_under = entry1.completion_item.label:find("^_+")
+				local _, entry2_under = entry2.completion_item.label:find("^_+")
+				entry1_under = entry1_under or 0
+				entry2_under = entry2_under or 0
+				if entry1_under > entry2_under then
+					return false
+				elseif entry1_under < entry2_under then
+					return true
+				end
+			end,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
+	experimental = {
+		-- I like the new menu better! Nice work hrsh7th
+		native_menu = false,
+
+		-- Let's play with this for a day or two
+		ghost_text = false,
+	},
 })
 
 -- Set configuration for specific filetype.
---
+++ lua/cmp-config/luasnip.lua
@@ -0,0 +1,52 @@
+local status, ls = pcall(require, "luasnip")
+if not status then
+	vim.notify("没有找到 luasnip")
+	return
+end
+
+local types = require("luasnip.util.types")
+
+ls.config.set_config({
+	-- This tells LuaSnip to remember to keep around the last snippet.
+	-- You can jump back into it even if you move outside of the selection
+	history = false,
+
+	-- This one is cool cause if you have dynamic snippets, it updates as you type!
+	updateevents = "TextChanged,TextChangedI",
+
+	-- Autosnippets:
+	enable_autosnippets = true,
+
+	-- Crazy highlights!!
+	-- #vid3
+	-- ext_opts = nil,
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { " « ", "NonTest" } },
+			},
+		},
+	},
+})
+
+-- this will expand the current item or jump to the next item within the snippet.
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
+end, { silent = true })
+
+-- this always moves to the previous item within the snippet
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
+end, { silent = true })
+
+-- <c-l> is selecting within a list of options.
+-- This is useful for choice nodes (introduced in the forthcoming episode 2)
+vim.keymap.set("i", "<c-l>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end)

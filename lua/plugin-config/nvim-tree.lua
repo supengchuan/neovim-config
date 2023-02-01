6f71e2e328a86a5690b4783b0c409cae29a82b22
supengchuan <supengchuan@qq.com>
2023-02-01 20:13:29 +0800

    change nvim-tree config

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,10 +1,14 @@
 local status, nvim_tree = pcall(require, "nvim-tree")
 if not status then
-    vim.notify("没有找到 nvim-tree")
-    return
+	vim.notify("没有找到 nvim-tree")
+	return
 end
 
 nvim_tree.setup({
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
 	sort_by = "case_sensitive",
 	view = {
 		adaptive_size = false,

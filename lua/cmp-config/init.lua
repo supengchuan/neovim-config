055a71a2a12ad817729e45e0e12e2f5f8f1612aa
supengchuan <supengchuan@qq.com>
2023-05-11 09:47:48 +0800

    change dracula background color

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -76,17 +76,17 @@ cmp.setup({
 		end,
 	},
 	-- do not enable auto-completion in comments
-	enabled = function()
-		-- disable completion in comments
-		local context = require("cmp.config.context")
-		-- keep command mode completion enabled when cursor is in a comment
-		if vim.api.nvim_get_mode().mode == "c" then
-			return true
-		else
-			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-			-- return not context.in_syntax_group("Comment")
-		end
-	end,
+	--enabled = function()
+	--	-- disable completion in comments
+	--	local context = require("cmp.config.context")
+	--	-- keep command mode completion enabled when cursor is in a comment
+	--	if vim.api.nvim_get_mode().mode == "c" then
+	--		return true
+	--	else
+	--		return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+	--		-- return not context.in_syntax_group("Comment")
+	--	end
+	--end,
 
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -2,6 +2,13 @@ vim.o.background = "dark"
 
 local colorscheme = ""
 
+-- override Dracula background color
+require("dracula").setup({
+	colors = {
+		bg = "#1e1f29",
+	},
+})
+
 colorscheme = "dracula"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then

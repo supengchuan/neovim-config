f5a857bf7b415ff4f8bb890362d5907fb7894e0b
supengchuan <supengchuan@datacloak.com>
2023-08-02 19:51:42 +0800

    auto close aerial when jump to select

--- lua/plugin-config/aerial.lua
+++ lua/plugin-config/aerial.lua
@@ -158,10 +158,10 @@ require("aerial").setup({
 	post_jump_cmd = "normal! zz",
 
 	-- When true, aerial will automatically close after jumping to a symbol
-	close_on_select = false,
+	close_on_select = true,
 
 	-- Show box drawing characters for the tree hierarchy
-	show_guides = false,
+	show_guides = true,
 
 	-- The autocmds that trigger symbols update (not used for LSP backend)
 	update_events = "TextChanged,InsertLeave",

d5b0604f95c48183c7cf28c15307e60445b98193
supengchuan <supengchuan@datacloak.com>
2023-09-25 16:46:48 +0800

    change color to gruvbox

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -12,7 +12,7 @@ require("dracula").setup({
 colorscheme = "dracula"
 
 colorscheme = "gruvbox"
-colorscheme = "tokyonight"
+--colorscheme = "tokyonight"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	vim.notify("colorscheme: " .. colorscheme .. " 没有找到！")

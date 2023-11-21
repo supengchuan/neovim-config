2e3391be1e3fd4280af736362f73159bcca890d7
苏鹏川 <supengchuan@datacloak.com>
2023-11-21 10:20:39 +0800

    set tokyonight theme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -14,6 +14,25 @@ colorscheme = "dracula"
 colorscheme = "gruvbox"
 colorscheme = "catppuccin-frappe"
 colorscheme = "tokyonight-moon"
+
+local prefix = "tokyonight"
+if string.sub(colorscheme, 1, #prefix) == prefix then
+	require("tokyonight").setup({
+		transparent = true, -- Enable this to disable setting the background colors
+		styles = {
+			-- Style to be applied to different syntax groups
+			-- Value is any valid attr-list value for `:help nvim_set_hl`
+			comments = { italic = true },
+			keywords = { italic = true },
+			functions = {},
+			variables = {},
+			-- Background styles. Can be "dark", "transparent" or "normal"
+			sidebars = "transparent", -- style for sidebars, see below
+			floats = "transparent", -- style for floating windows
+		},
+	})
+end
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	vim.notify("colorscheme: " .. colorscheme .. " 没有找到！")

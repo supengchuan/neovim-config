5db125d0e2d9d144db0e3eb80913c650bcf507ca
苏鹏川 <supengchuan@datacloak.com>
2024-02-20 16:09:19 +0800

    fix go lsp config

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -7,7 +7,25 @@ local M = {
 	},
 	config = function()
 		-- set config
-		require("lsp-config.go-tool")
+		local lsp_common = require("lsp-config.common")
+		require("go").setup({
+			tag_transform = "camelcase",
+			lsp_cfg = {
+				capabilities = lsp_common.capabilities,
+				-- other setups
+
+				handlers = lsp_common.handlers,
+				on_attach = function(client, bufnr)
+					lsp_common.keyAttach(bufnr)
+				end,
+			},
+			lsp_inlay_hints = {
+				only_current_line = true,
+				parameter_hints_prefix = "",
+			},
+
+			lsp_keymaps = false,
+		})
 	end,
 	--event = { "CmdlineEnter" },
 	ft = { "go", "gomod" },

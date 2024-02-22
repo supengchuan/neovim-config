c01f1442cd59d5175b70f329d827b09b2de8e37e
supengchuan <supengchuan@qq.com>
2024-02-22 11:38:02 +0800

    add mason auto install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,16 +1,46 @@
 local M = {
 	"williamboman/mason.nvim",
+	--	dependencies = { "williamboman/mason-lspconfig.nvim" },
 	event = "VimEnter",
-	config = {
-		ui = {
-			border = "double",
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
+	config = function()
+		require("mason").setup({
+			ui = {
+				border = "double",
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
 			},
-		},
-	},
-}
+		})
 
+		local registry = require("mason-registry")
+		local others = {
+			-- lsp
+			"bash-language-server",
+			"clangd",
+			"gopls",
+			"lua-language-server",
+			"nginx-language-server",
+			"rust-analyzer",
+
+			-- formatter
+			"stylua",
+			"goimports",
+			"shfmt",
+
+			-- linter
+			"shellcheck",
+		}
+
+		for _, pkg_name in ipairs(others) do
+			local ok, pkg = pcall(registry.get_package, pkg_name)
+			if ok then
+				if not pkg:is_installed() then
+					pkg:install()
+				end
+			end
+		end
+	end,
+}
 return M

40794c24ba726ec1277fd98878f5440262724716
supengchuan <supengchuan@datacloak.com>
2024-05-22 14:11:21 +0800

    change golang lsp config for inlay hints

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -17,54 +17,17 @@ local M = {
 				handlers = lsp_common.handlers,
 				on_attach = function(client, bufnr)
 					lsp_common.keyAttach(bufnr)
+					if client.server_capabilities.inlayHintProvider then
+						vim.lsp.inlay_hint.enable(true)
+					end
 				end,
 			},
 			lsp_inlay_hints = {
-				enable = true,
-				style = "eol", -- 'default: inlay', 'eol': show at end of line, 'inlay': show in the middle of the line
-
-				-- Note: following setup only for for style == 'eol'
-				-- Only show inlay hints for the current line
-				only_current_line = false,
-
-				-- Event which triggers a refresh of the inlay hints.
-				-- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
-				-- not that this may cause higher CPU usage.
-				-- This option is only respected when only_current_line and
-				-- autoSetHints both are true.
-				only_current_line_autocmd = "CursorHold",
-
-				-- whether to show variable name before type hints with the inlay hints or not
-				-- default: false
-				show_variable_name = true,
-
-				-- prefix for parameter hints
-				parameter_hints_prefix = "󰊕 ",
-				show_parameter_hints = true,
-
-				-- prefix for all the other hints (type, chaining)
-				-- default: "=>"
-				other_hints_prefix = "=> ",
-
-				-- whether to align to the length of the longest line in the file
-				max_len_align = false,
-
-				-- padding from the left if max_len_align is true
-				max_len_align_padding = 1,
-
-				-- whether to align to the extreme right or not
-				right_align = false,
-
-				-- padding from the right if right_align is true
-				right_align_padding = 6,
-
-				-- The color of the hints
-				highlight = "Comment",
+				enable = false,
 			},
 
 			lsp_keymaps = false,
 			comment_placeholder = "  ",
-			diagnostic = false,
 		})
 	end,
 	--event = { "CmdlineEnter" },

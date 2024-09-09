0ffddfc6abb243e22a9c31f3b7e0a7491830ebbf
苏鹏川 <supengchuan@datacloak.com>
2024-09-09 10:28:08 +0800

    fix lsp not load custom config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -42,49 +42,39 @@ servers.nginx_language_server = function()
 end
 
 servers.lua_ls = function()
-  -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-  local runtime_path = vim.split(package.path, ";")
-  table.insert(runtime_path, "lua/?.lua")
-  table.insert(runtime_path, "lua/?/init.lua")
-
-  local opts = {
-    settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = "LuaJIT",
-          -- Setup your lua path
-          path = runtime_path,
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = { "vim" },
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-          checkThirdParty = false,
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-    capabilities = require("lsp-config.common").capabilities,
-    on_attach = function(client, bufnr)
-      local lspComm = require("lsp-config.common")
-      lspComm.keyAttach(bufnr)
-      lspComm.disableFormat(client)
-      lspComm.shwLinDiaAtom(bufnr)
-      -- lspComm.hlSymUdrCursor(client, bufnr)
-    end,
-    handlers = require("lsp-config.common").handlers,
-  }
-
   return {
     on_setup = function(server)
-      server.setup(opts)
+      server.setup({
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              },
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            },
+          })
+        end,
+        settings = {
+          Lua = {},
+        },
+      })
     end,
   }
 end
@@ -139,6 +129,7 @@ servers.bufls = function()
     end,
     handlers = require("lsp-config.common").handlers,
   }
+
   return {
     on_setup = function(server)
       server.setup(opts)
@@ -161,9 +152,9 @@ local M = {
     end
 
     for name, config in pairs(servers) do
-      if config ~= nil and type(config) == "table" then
+      if config ~= nil and type(config) == "function" then
         -- 自定义初始化配置文件必须实现on_setup 方法
-        config.on_setup(lspconfig[name])
+        config().on_setup(lspconfig[name])
       else
         -- 使用默认参数
         lspconfig[name].setup({})

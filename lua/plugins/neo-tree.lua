6224ff26c401c3f4a002007244a1571cefb12341
苏鹏川 <supengchuan@datacloak.com>
2024-10-28 15:52:39 +0800

    neo-tree add follow current file config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,4 +7,15 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
+    })
+  end,
 }

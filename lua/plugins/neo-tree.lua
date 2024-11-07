66c75adaa45be1fe79072c7a8e40ab211e8f1f1e
苏鹏川 <supengchuan@datacloak.com>
2024-11-07 17:54:57 +0800

    fix neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,74 +10,56 @@ return {
   deactivate = function()
     vim.cmd([[Neotree close]])
   end,
-  init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
-    vim.api.nvim_create_autocmd("BufEnter", {
-      group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
-      desc = "Start Neo-tree with directory",
-      once = true,
-      callback = function()
-        if package.loaded["neo-tree"] then
-          return
-        else
-          local stats = vim.uv.fs_stat(vim.fn.argv(0))
-          if stats and stats.type == "directory" then
-            require("neo-tree")
-          end
-        end
-      end,
-    })
-  end,
-  opts = {
-    sources = { "filesystem", "buffers", "git_status" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = {
-        enabled = true, -- This will find and focus the file in the active buffer every time
-      },
-      use_libuv_file_watcher = true,
-    },
-    window = {
-      mappings = {
-        ["l"] = "open",
-        ["h"] = "close_node",
-        ["<space>"] = "none",
-        ["Y"] = {
-          function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
-          end,
-          desc = "Copy Path to Clipboard",
-        },
-        ["O"] = {
-          function(state)
-            require("lazy.util").open(state.tree:get_node().path, { system = true })
-          end,
-          desc = "Open with System Application",
-        },
-        ["P"] = { "toggle_preview", config = { use_float = false } },
-      },
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-      git_status = {
-        symbols = {
-          unstaged = "󰄱",
-          staged = "󰱒",
-        },
-      },
-    },
-  },
+  opts = {},
   config = function()
+    require("neo-tree").setup({
+      sources = { "filesystem", "buffers", "git_status" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+        },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["<space>"] = "none",
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+          ["O"] = {
+            function(state)
+              require("lazy.util").open(state.tree:get_node().path, { system = true })
+            end,
+            desc = "Open with System Application",
+          },
+          ["P"] = { "toggle_preview", config = { use_float = false } },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        git_status = {
+          symbols = {
+            unstaged = "󰄱",
+            staged = "󰱒",
+          },
+        },
+      },
+    })
     vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { link = "QuickFixLine" })
   end,
 }

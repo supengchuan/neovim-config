49677834d42cb56f70b05cf2b604353a7f8ab940
苏鹏川 <supengchuan@datacloak.com>
2024-12-30 18:01:38 +0800

    change vim.loop to vim.uv

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -47,7 +47,7 @@ servers.lua_ls = function()
       server.setup({
         on_init = function(client)
           local path = client.workspace_folders[1].name
-          if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
             return
           end
 

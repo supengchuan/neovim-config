a1339e93dc0521cd923ce7cd0b55cf4ddf6ef26e
苏鹏川 <supengchuan@datacloak.com>
2025-01-03 14:40:09 +0800

    optimize keymap

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -11,26 +11,20 @@ local M = {
   opts = {
     preset = "modern",
   },
+  -- stylua: ignore
   keys = {
-    {
-      "<leader>/",
-      function()
-        require("which-key").show({ global = true })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
+    { "<leader>/", function() require("which-key").show({ global = true }) end, desc = "Buffer Local Keymaps (which-key)" },
     --common keymaps
-    { "jj", "<ESC>", desc = "double j to normal mode", mode = "i" },
     { "<leader>+", "<cmd>vertical resize -10<CR>", desc = "current buffer narrower" },
-    { "<leader><CR>", require("utils").ToggleWrap, desc = "set file wrap or no wrap" },
     { "<leader>=", "<cmd>vertical resize +10<CR>", desc = "current buffer wider" },
+    { "<leader><CR>", require("utils").ToggleWrap, desc = "set file wrap or no wrap" },
     { "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", desc = "go to previous diagnostic" },
     { "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", desc = "go to next diagnostic" },
     { "<leader>h", require("utils").ToggleInlayHints, desc = "set a buffer enable inlay hints or not" },
     { "<leader>i", "<cmd>lua vim.diagnostic.open_float()<CR>", desc = "open diagnostic as float window" },
     { "<leader>j", "5j", desc = "move cursor down five lines" },
     { "<leader>k", "5k", desc = "move cursor up five lines" },
-    { "<leader>nl", "<cmd>nohlsearch<CR>", desc = "cancel highlight for search" },
+    { ";n", "<cmd>nohlsearch<CR>", desc = "cancel highlight for search" },
     { "<leader>q", "<cmd>q<CR>", desc = "quit from current buffer" },
     { "<leader>Q", "<cmd>qa!<CR>", desc = "force quit all" },
     { "<leader>w", "<cmd>w<CR>", desc = "save current buffer" },

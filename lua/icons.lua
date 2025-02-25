48915468ecfcf5864167c181d6b17612c4e324b8
苏鹏川 <supengchuan@datacloak.com>
2025-02-25 16:20:45 +0800

    config aerial

--- lua/icons.lua
+++ lua/icons.lua
@@ -55,5 +55,32 @@ M.local_icons = {
   Operator = "󰆕",
   TypeParameter = "󰅲",
 }
+M.icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
 
 return M
--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -17,7 +17,7 @@ local M = {
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+--    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
   },
 }
 

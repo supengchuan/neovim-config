5c2f18613bd62ab4fa8f0b609b755938483e8fee
苏鹏川 <supengchuan@datacloak.com>
2025-03-11 14:49:44 +0800

    add markview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,26 +1,32 @@
 -- markdown
 -- install without yarn or npm
 local M = {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = "npm install && rm package-lock.json && git restore .",
-  config = function()
-    vim.g.mkdp_open_to_the_world = 1
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "npm install && rm package-lock.json && git restore .",
+    config = function()
+      vim.g.mkdp_open_to_the_world = 1
 
-    local local_ip = "127.0.0.1"
-    local on_linux = vim.uv.os_uname().sysname:match("Linux")
+      local local_ip = "127.0.0.1"
+      local on_linux = vim.uv.os_uname().sysname:match("Linux")
 
-    if on_linux then
-      local_ip = vim.fn.system("hostname -I | awk '{print $1}'")
-    end
+      if on_linux then
+        local_ip = vim.fn.system("hostname -I | awk '{print $1}'")
+      end
 
-    vim.g.mkdp_open_ip = local_ip
-    vim.g.mkdp_port = "9414"
-    vim.g.mkdp_echo_preview_url = 1
-  end,
-  keys = {
-    { "<leader>p", "<cmd>MarkdownPreviewToggle<CR>", desc = "preview markdown" },
+      vim.g.mkdp_open_ip = local_ip
+      vim.g.mkdp_port = "9414"
+      vim.g.mkdp_echo_preview_url = 1
+    end,
+    keys = {
+      { "<leader>p", "<cmd>MarkdownPreviewToggle<CR>", desc = "preview markdown" },
+    },
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown" },
   },
 }
 

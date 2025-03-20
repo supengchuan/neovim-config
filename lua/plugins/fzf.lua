5e131eb3db4e56392cc31bc96dc3b0a4ec5f57d1
苏鹏川 <supengchuan@datacloak.com>
2025-03-20 11:51:19 +0800

    config fzf-lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,9 +2,25 @@ local M = {
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = "FzfLua",
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
-  opts = {},
+  opts = function(_, opts)
+    local fzf = require("fzf-lua")
+    local config = fzf.config
+    local actions = fzf.actions
+
+    -- send search result to quickfix list
+    config.defaults.keymap.fzf["ctrl-q"] = "select-all+accept"
+    config.defaults.keymap.fzf["ctrl-u"] = "half-page-up"
+    config.defaults.keymap.fzf["ctrl-d"] = "half-page-down"
+    config.defaults.keymap.fzf["ctrl-x"] = "jump"
+    config.defaults.keymap.fzf["ctrl-f"] = "preview-page-down"
+    config.defaults.keymap.fzf["ctrl-b"] = "preview-page-up"
+    config.defaults.keymap.builtin["<c-f>"] = "preview-page-down"
+    config.defaults.keymap.builtin["<c-b>"] = "preview-page-up"
+    return {}
+  end,
 }
 
 return M

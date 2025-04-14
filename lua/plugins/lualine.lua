4bdf567658f2fd43c018594ead5408dd0c242100
苏鹏川 <supengchuan@datacloak.com>
2025-04-14 10:29:39 +0800

    config lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,53 +1,8 @@
-local function customTheme()
-  local colors = {
-    darkgray = "#16161d",
-    gray = "#727169",
-    innerbg = nil,
-    outerbg = "#16161D",
-    normal = "#7e9cd8",
-    insert = "#98bb6c",
-    visual = "#ffa066",
-    replace = "#e46876",
-    command = "#e6c384",
-  }
-  return {
-    inactive = {
-      a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
-      c = { fg = colors.gray, bg = colors.innerbg },
-    },
-    visual = {
-      a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
-      c = { fg = colors.gray, bg = colors.innerbg },
-    },
-    replace = {
-      a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
-      c = { fg = colors.gray, bg = colors.innerbg },
-    },
-    normal = {
-      a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
-      c = { fg = colors.gray, bg = colors.innerbg },
-    },
-    insert = {
-      a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
-      c = { fg = colors.gray, bg = colors.innerbg },
-    },
-    command = {
-      a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
-      c = { fg = colors.gray, bg = colors.innerbg },
-    },
-  }
-end
-
 local M = {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "folke/noice.nvim",
   },
   event = "VeryLazy",
   opts = {
@@ -118,7 +73,7 @@ local M = {
       lualine_a = { "mode" },
       lualine_b = { "searchcount", "selectioncount" },
       lualine_c = { "windows" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_x = { "lsp_status" },
       lualine_y = { "filename" },
       lualine_z = { "mode" },
     },

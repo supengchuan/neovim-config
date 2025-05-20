5aa904555f6aa96492369d6b7d002882f6e3db3b
苏鹏川 <supengchuan@datacloak.com>
2025-05-20 11:50:48 +0800

    add quick chose quickfix item when quickfix window is opened

--- plugin/keymap.lua
+++ plugin/keymap.lua
@@ -14,3 +14,50 @@ vim.keymap.set("n", "<leader>;", function()
   vim.cmd("startinsert!") -- Enter insert mode after the line is opened
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
 end, { desc = "Add semicolon if needed and open new line" })
+
+---@param next boolean
+local function choseQuickfixorMoveCursor(next)
+  local cmd = "cnext"
+  local position = "bottom"
+  if not next then
+    cmd = "cprev"
+    position = "top"
+  end
+
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
+    if buftype == "quickfix" then
+      ---@type boolean, string?
+      local ok, err = pcall(function()
+        vim.cmd(cmd)
+      end)
+      if not ok then
+        if err and string.match(err, "E553") then
+          vim.notify("You're already at the " .. position .. " of quickfix item", vim.log.levels.INFO)
+        elseif err then
+          vim.notify("chose quickfix error: " .. err, vim.log.levels.WARN)
+        else
+          vim.notify("chose quickfix error: unknown error", vim.log.levels.ERROR)
+        end
+      end
+      return
+    end
+  end
+
+  if cmd == "cnext" then
+    vim.cmd("normal! 5j")
+  else
+    vim.cmd("normal! 5k")
+  end
+end
+
+-- if quickfix window is opened, just chose next quickfix item
+-- or move cursor 5 lines below
+vim.keymap.set("n", "<leader>j", function()
+  choseQuickfixorMoveCursor(true)
+end, { silent = true, desc = "next quickfix or mvoe down 5 lines" })
+
+vim.keymap.set("n", "<leader>k", function()
+  choseQuickfixorMoveCursor(false)
+end, { silent = true, desc = "prev quickfix or move up 5 lines " })

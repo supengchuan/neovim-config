7d99e34ecc3b9bb9758e4866b497e544d3894c24
苏鹏川 <supengchuan@datacloak.com>
2025-08-06 11:46:36 +0800

    add vue+ts lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,4 +1,33 @@
 local servers = {
+  vtsls = {
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+    settings = {
+      vtsls = { tsserver = { globalPlugins = {} } },
+      typescript = {
+        inlayHints = {
+          parameterNames = { enabled = "literals" },
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
+    },
+    before_init = function(_, config)
+      table.insert(config.settings.vtsls.tsserver.globalPlugins, {
+        name = "@vue/typescript-plugin",
+        location = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server"),
+        languages = { "vue" },
+        configNamespace = "typescript",
+        enableForWorkspaceTypeScriptVersions = true,
+      })
+    end,
+    on_attach = function(client)
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+  },
   bashls = {},
   protols = {},
   pyright = {},
@@ -73,6 +102,7 @@ local mason_extra_tools = {
 
   -- lsp
   "rust-analyzer",
+  "vue-language-server",
   "gopls",
 }
 

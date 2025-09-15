20aca631033a64fb316cfa58e6cd4df390373c96
苏鹏川 <supengchuan@datacloak.com>
2025-09-15 16:37:17 +0800

    add acp in code companion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -28,21 +28,56 @@ local M = {
       inline = {
         adapter = "gemini",
       },
+      cmd = {
+        adapter = "gemini_cli",
+      },
     },
-    adapter = {
-      gemini = function()
-        require("codecompanion.adapters").extend("gemini", {
-          env = {
-            api_key = "GEMINI_API_KEY",
-          },
-        })
-      end,
+    adapters = {
+      http = {
+        opts = {
+          show_defaults = false, -- hiding default adapters
+        },
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = "GEMINI_API_KEY",
+            },
+          })
+        end,
+      },
+      acp = {
+        opts = {
+          show_defaults = false, -- hiding default adapters
+        },
+        gemini_cli = function()
+          return require("codecompanion.adapters").extend("gemini_cli", {
+            defaults = {
+              auth_method = "gemini-api-key", -- "oauth-personal"|"gemini-api-key"|"vertex-ai"
+            },
+            env = {
+              api_key = "GEMINI_API_KEY",
+            },
+          })
+        end,
+      },
     },
     display = {
       chat = {
         window = {
           position = "right", -- left|right|top|bottom (nil will default depending on vim.opt.splitright|vim.opt.splitbelow)
           width = 0.3,
+        },
+      },
+
+      action_palette = {
+        width = 80,
+        height = 10,
+        prompt = "Prompt ", -- Prompt used for interactive LLM calls
+        provider = "snacks", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+        opts = {
+          show_default_actions = true, -- Show the default actions in the action palette?
+          show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+          title = "CodeCompanion Actions", -- The title of the action palette
         },
       },
     },

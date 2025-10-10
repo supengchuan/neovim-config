9f42d21a60b1be25f051f27c7c332d30ccadfb10
苏鹏川 <supengchuan@datacloak.com>
2025-10-10 16:49:22 +0800

    add diagnostics

---
+++ lua/plugins/diagnostics.lua
@@ -0,0 +1,11 @@
+local M = {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy",
+  priority = 1000,
+  config = function()
+    require("tiny-inline-diagnostic").setup()
+    vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
+  end,
+}
+
+return M

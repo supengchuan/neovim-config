1f9f5dcfe9a2ddcb6a12e3f07bdbe9296e58f60a
苏鹏川 <supengchuan@datacloak.com>
2025-10-10 17:08:59 +0800

    config diagnostics

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -3,8 +3,29 @@ local M = {
   event = "VeryLazy",
   priority = 1000,
   config = function()
-    require("tiny-inline-diagnostic").setup()
-    vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
+    require("tiny-inline-diagnostic").setup({
+      -- Style preset for diagnostic messages
+      -- Available options: "modern", "classic", "minimal", "powerline", "ghost", "simple", "nonerdfont", "amongus"
+      preset = "ghost",
+
+      options = {
+        show_source = {
+          enabled = true,
+          if_many = true,
+        },
+        -- Configuration for breaking long messages into separate lines
+        break_line = {
+          -- Enable breaking messages after a specific length
+          enabled = true,
+
+          -- Number of characters after which to break the line
+          after = 50,
+        },
+      },
+    })
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
   end,
 }
 
--- plugin/diagnostics.lua
+++ plugin/diagnostics.lua
@@ -1,9 +1,10 @@
 vim.diagnostic.config({
-  virtual_text = {
-    source = "if_many", -- Or "if_many"
-    prefix = "●", -- Could be '■', '▎', 'x'
-    spacing = 20,
-  },
+  virtual_text = false,
+  -- virtual_text = {
+  --   source = "if_many", -- Or "if_many"
+  --   prefix = "●", -- Could be '■', '▎', 'x'
+  --   spacing = 20,
+  -- },
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = " ",

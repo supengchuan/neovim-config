42a6e8c79c840474c43b6b62fcbd8d85fbadf251
苏鹏川 <supengchuan@datacloak.com>
2025-12-02 17:11:39 +0800

    change tiny diagnostics configs

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -6,7 +6,7 @@ local M = {
     require("tiny-inline-diagnostic").setup({
       -- Style preset for diagnostic messages
       -- Available options: "modern", "classic", "minimal", "powerline", "ghost", "simple", "nonerdfont", "amongus"
-      preset = "ghost",
+      preset = "powerline",
 
       options = {
         add_messages = {
@@ -19,13 +19,15 @@ local M = {
           enabled = true,
           if_many = true,
         },
+        -- Color the arrow to match the severity of the first diagnostic
+        set_arrow_to_diag_color = true,
+
         -- Configuration for breaking long messages into separate lines
         break_line = {
           -- Enable breaking messages after a specific length
           enabled = true,
-
           -- Number of characters after which to break the line
-          after = 50,
+          after = 80,
         },
       },
     })

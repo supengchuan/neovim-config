766a3aa2dc86d06c3fdad4bccbec3ce75cfbd8c4
supengchuan <supc@supengchuandeMacBook-Pro.local>
2025-12-16 20:04:10 +0800

    change nvim-treesitter to main branch

---
+++ lua/plugins/treesitters.lua
@@ -0,0 +1,74 @@
+local M = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      -- put your config here
+      require("nvim-treesitter").install({
+        "rust",
+        "javascript",
+        "c",
+        "lua",
+        "vim",
+        "go",
+        "toml",
+        "vimdoc",
+        "yaml",
+        "bash",
+        "sql",
+        "proto",
+        "css",
+        "markdown",
+        "json",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+      -- vim.g.no_ruby_maps = true
+      -- vim.g.no_rust_maps = true
+      -- vim.g.no_go_maps = true
+    end,
+    config = function() end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    config = function()
+      require("treesitter-context").setup({
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        multiwindow = false, -- Enable multiwindow support.
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+    end,
+    keys = {
+      --{
+      --  "[c",
+      --  function()
+      --    require("treesitter-context").go_to_context(vim.v.count1)
+      --  end,
+      --  desc = "jump to context(upwards)",
+      --},
+    },
+  },
+}
+return M

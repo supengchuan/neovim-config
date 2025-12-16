435278da4a5a3bd159ebb703e55412a3ed361a73
supengchuan <supc@supengchuandeMacBook-Pro.local>
2025-12-16 12:57:01 +0800

    use old nvim treesitter

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -7,6 +7,7 @@ local M = {
       "nvim-treesitter/nvim-treesitter-textobjects",
       "nvim-treesitter/playground",
     },
+    branch = "master",
     build = ":TSUpdate",
     event = "VeryLazy",
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline

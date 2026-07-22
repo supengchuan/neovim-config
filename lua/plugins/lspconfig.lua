c5ebfa75bf3a563954f968201c9e445a04be62fa
supengchuan <supengchuan@bytedance.com>
2026-07-22 10:45:44 +0800

    fix: skip gopls for virtual buffers

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -101,6 +101,13 @@ local servers = {
     },
   },
   gopls = {
+    root_dir = function(bufnr, on_dir)
+      if not vim.startswith(vim.uri_from_bufnr(bufnr), "file://") then
+        return
+      end
+
+      on_dir(vim.fs.root(bufnr, { "go.work", "go.mod", ".git" }))
+    end,
     settings = {
       gopls = {
         -- Important: make gopls faster
